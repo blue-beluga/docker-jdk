a826f393f8913cce6b0de32d6af2aafbb9ad905f
Stefano Harding <riddopic@gmail.com>
2016-07-13 21:23:40 -0700

    docker-jdk

---
+++ versions/7/config.mk
@@ -0,0 +1,8 @@
+
+export JDK_VERSION = 7
+export JDK_UPDATE = 80
+export JDK_BUILD = 15
+export JDK_PACKAGE = jdk
+export JDK_SHA256 = bad9a731639655118740bee119139c1ed019737ec802a630dd7ad7aab4309623
+export JDK_HOME = /opt/jdk
+export FROM = bluebeluga/glibc
---
+++ versions/8/config.mk
@@ -0,0 +1,8 @@
+
+export JDK_VERSION = 8
+export JDK_UPDATE = 92
+export JDK_BUILD = 14
+export JDK_PACKAGE = jdk
+export JDK_SHA256 = 79a3f25e9b466cb9e969d1772ea38550de320c88e9119bf8aa11ce8547c39987
+export JDK_HOME = /opt/jdk
+export FROM = bluebeluga/glibc
